e9deb35e2e9185430c492f4f72085a818f86db23
Gael Santome Lopez <you@example.comgaelsantome@gmail.com git config --global user.name Gasel>
2024-10-31 15:48:48 +0100

    Ejercicio 30 finalizado

--- src/main/java/com/campusdual/classroom/Exercise30.java
+++ src/main/java/com/campusdual/classroom/Exercise30.java
@@ -1,12 +1,23 @@
 package com.campusdual.classroom;
 
+
 public class Exercise30 {
 
-    public static int divisionWithCustomException(int dividend, int divisor){
 
+    public static double divisionWithCustomException(double numerator, double denominator) throws DivisionByZeroException {
+        if (denominator == 0) {
+            throw new DivisionByZeroException("No se pueden dividir");
+        }
+        return numerator / denominator;
     }
 
     public static void main(String[] args) {
-
+        try {
+            double result = divisionWithCustomException(10, 2);
+            System.out.println("Resultado" + result);
+        } catch (DivisionByZeroException e) {
+            System.out.println( e.getMessage());
+        }
     }
 }
+
